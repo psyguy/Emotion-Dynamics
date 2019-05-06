17ee6fc4c4e59d27c16e42ae7d1a830d30b7f0d5
Manuel Haqi <mhscientist@gmail.com>
2019-05-06 20:24:24 +0200

    Bimodality corrected!

---
+++ Codes/correcting-bimodality.R
@@ -0,0 +1,65 @@
+# rm(list=ls())
+load("I:/Emotion Dynamics Clean/loadings.within.RData")
+load("I:/Emotion Dynamics Clean/loadings.between.RData")
+
+loadings.between <- loadings.between %>% llply(function(x){
+  colnames(x)[1:2] <- c("dataset name", "model definition")
+  return(x)})
+
+loadings.within <- loadings.within %>% llply(function(x){
+  colnames(x)[1:2] <- c("dataset name", "model definition")
+  return(x)})
+
+list.of.names <- list('CLINICAL ESM','Cogito','ELISE ESM14', 'JULIAN EGON',
+                      'KATHLEEN STRESSKLINIEK', 'Laura ESM 2014','Laura ESM 2016',
+                      'LONGITUDINAL W1', 'LONGITUDINAL W2', 'LONGITUDINAL W3','MARLIES BPD',
+                      'MDD BPD TRULL', 'MDD GOTLIB', 'MTURK DAILY DIARY',
+                      'PETEMADDY')
+
+my.little.align <- function(d){
+  
+  items <- d$item %>% unique()
+  factors <- colnames(d)[5:(ncol(d)-1)]
+  d$seed <- d$seed %>% as.character() %>% as.numeric()
+  new.fac.loadings.all <- c()
+  for(n.f in 1:length(factors)){
+    # d.new <- d %>% pull(f) %>% as.character() %>% as.numeric()
+    f <- 4 + n.f
+    new.fac.loadings <- c()
+    for(s in 1:100){
+      fac.loadings <- d %>% filter(seed == s) %>%
+        pull(f) %>% as.character() %>% as.numeric()
+      first.sign <- (fac.loadings[fac.loadings!=0])[1] %>% sign()
+      fac.loadings <- fac.loadings*first.sign
+      new.fac.loadings <- new.fac.loadings %>% c(fac.loadings)
+    }
+    # new.fac.loadings <- new.fac.loadings %>% cbind(fac.loadings)
+    d[,f] <- new.fac.loadings
+  }
+  d %>% return()
+}
+
+
+my.loading.aligner <- function(l){
+  
+  # l <- loadings.list# <- loadings.within[[1]]
+  
+  names.datasets <- l$`dataset name` %>% unique() %>% as.character()
+  # name.dataset <- names.datasets[[1]]
+  
+  d <- NULL
+  for(name.dataset in names.datasets){
+    dd <- l %>% filter(`dataset name` == name.dataset)
+    d <- d %>% rbind(my.little.align(dd))
+  }
+  
+  d %>% return()
+  }
+
+Sys.time()
+corrected.loadings.within <- loadings.within %>% llply(my.loading.aligner)
+corrected.loadings.between <- loadings.between %>% llply(my.loading.aligner)
+Sys.time()
+
+# save(corrected.loadings.within, file = "corrected.loadings.within.RData")
+# save(corrected.loadings.between, file = "corrected.loadings.between.RData")
---
+++ Codes/spaghetti-diagnosis.R
@@ -0,0 +1,90 @@
+library(yarrr)
+
+my.spaghetti.ploter <- function(d, name.dataset, wbp){
+  
+  # loadings.list <- loadings.within
+  # loading.element <- loadings.list$def.C.bare -> d
+
+  # d <- loadings.between$def.B.cov
+  # name.dataset <- "PETEMADDY"# list.of.name
+  
+  colnames(d)[1:2] <- c("dataset name", "model definition")
+  # d <- d %>% filter(`dataset name` == name.dataset)#"MDD BPD TRULL")#|`dataset name` =="MDD BPD TRULL")
+  d <- d %>% filter(`dataset name` == name.dataset)#"CLINICAL ESM")#|`dataset name` =="PETEMADDY")
+  d.numeric <- d[,-1:-4] %>%
+    lapply(function(x) as.numeric(as.character(x)))
+  d <- d[,1:4] %>% cbind(d.numeric)
+  
+  # par(las=2, mfrow = c(1,((ncol(d)-4))))
+  
+  for(i in 5:(ncol(d)-1)){
+    
+    name.factor <- colnames(d)[i]
+    def <- as.character(d$`model definition`[1])
+    # name.dataset <- as.character(d$`dataset name`[1])
+    
+    dd <- d %>% select(item, seed, (i)) # %>% filter(item %in% c("Happy", "Angry", "Sad"))
+    colnames(dd) <- c("item", "seed", "factor")
+    dd$factor <- dd$factor %>% as.character() %>% as.numeric()
+    
+    d.wide <- dd %>% spread(item, factor) %>% select(-seed)
+    
+    
+   
+    
+    
+    plot.title <- paste("Laodings of",
+                        name.factor,
+                        def,
+                        "for dataset",
+                        name.dataset
+    )
+    
+    ylab <- paste("Factor loading of", name.factor)
+    ylim <- c(-1,1)
+    threshold <- c(0.6,-0.6)
+    
+    g <- GGally::ggparcoord(d.wide,
+               title = plot.title,
+               # columns = c(1:8),
+               # groupColumn = '',
+               scale = 'globalminmax')
+    paste(wbp, name.dataset, def, name.factor, "png", sep = ".") %>% png()
+    print(g)
+    dev.off()
+    
+    # if(name.factor=="h2"){
+    #   ylab <- paste("Communality of items")
+    #   ylim <- c(0,1)
+    #   threshold <- 0.6}
+    
+    # paste(wbp, name.dataset, def, name.factor, "png", sep = ".") %>% png()
+    # par(las=1)
+    # pirateplot(formula = factor ~ `dataset name` + item,
+    #            main = plot.title,
+    #            cap.beans = F,
+    #            width.max = .1,
+    #            data = dd,           
+    #            ylim = ylim,
+    #            ylab = ylab,
+    #            xlab = "",
+    #            inf.method = 'sd',
+    #            avg.line.fun = mean,
+    #            theme = 1,
+    #            inf.disp = "line" # or "line"
+    # )
+    # abline(h = threshold, col = "red", lty=2, lwd=3)
+    # dev.off()
+    # print(g)
+  }
+  
+}
+
+
+for(dataset in list.of.names){
+  print(dataset)
+  print("within")
+  corrected.loadings.within %>% l_ply(my.spaghetti.ploter, dataset, "C.W")
+  print("now between")
+  corrected.loadings.between %>% l_ply(my.spaghetti.ploter, dataset, "C.B")
+}

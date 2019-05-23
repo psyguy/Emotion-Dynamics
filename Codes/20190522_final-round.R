bab8fb10330ea52b0dea2d01ead0bd6b40d9759e
Manuel Haqi <mhscientist@gmail.com>
2019-05-23 19:02:16 +0200

    22 May results added, should repeat stuff and get outputs

---
+++ Codes/20190522_final-round.R
@@ -0,0 +1,50 @@
+# reading the functions ---------------------------------------------------
+source("./Codes/My-Functions/my.library.loader.R")
+
+
+
+# reading appropriate samples ---------------------------------------------
+
+technique <- "r.sameocc"
+
+if(technique=="p.all") sampled.path <- "./22-May/p_all/"
+if(technique=="r.randocc") sampled.path <- "./22-May/r_randocc/"
+if(technique=="r.sameocc") sampled.path <- "./22-May/r_sameocc/"
+sampled.names <- list.files(path = sampled.path, pattern = "*.RData")
+
+
+
+## full list
+# list.of.names <- list('CLINICAL ESM','Cogito','ELISE ESM14', 'JULIAN EGON',
+#                       'KATHLEEN STRESSKLINIEK', 'Laura ESM 2014','Laura ESM 2016',
+#                       'LONGITUDINAL W1', 'LONGITUDINAL W2', 'LONGITUDINAL W3','MARLIES BPD',
+#                       'MDD BPD TRULL', 'MDD GOTLIB', 'MTURK DAILY DIARY', 
+#                       'PETEMADDY')
+
+## list of 12 datasets
+list.of.names <- list('CLINICAL ESM','ELISE ESM14', 'JULIAN EGON',
+                      'KATHLEEN STRESSKLINIEK', 'Laura ESM 2014','Laura ESM 2016',
+                      'LONGITUDINAL W1', 'LONGITUDINAL W2', 'LONGITUDINAL W3','MARLIES BPD',
+                      'MTURK DAILY DIARY', 
+                      'PETEMADDY')
+
+
+outcomes <- data.frame(matrix(nrow = 0, ncol = 8))
+failed.samples <- c()
+# colnames(outcomes) <- list.of.names
+
+t <- Sys.time()
+for(sampled in sampled.names){
+  load(paste(sampled.path, sampled, sep = ""))
+  if(!isS4(to.be.saved$model$def.A)){
+    failed.samples <- c(failed.samples, to.be.saved$name)
+    next()
+    }
+  outcome.single <- to.be.saved %>% my.mirt.comparison()
+  outcomes <- outcome.single %>% rbind(outcomes)
+}
+Sys.time() - t
+
+# outcomes.p.all <- outcomes
+# outcomes.r.randocc <- outcomes
+# outcomes.r.sameocc <- outcomes
--- Codes/My-Functions/my.vote.plotter.R
+++ Codes/My-Functions/my.vote.plotter.R
@@ -1,26 +1,27 @@
 my.vote.plotter <- function(outcomes, criterion = "AICc", sample.name = ""){
   
-  criterion <- criterion.list[6]
+  # criterion <- criterion.list[6]
+  sample.name <- technique
   ft <- outcomes %>% group_by_("dataset", criterion) %>% dplyr::summarise(freq=n())
 
   Frequency <- ft$freq
   Dataset <- ft$dataset
   Model <- ft[,2] %>% t() %>% as.vector()
 
-  # png(paste0(sample.name, "-", criterion, ".png"))
-  # ggplot(ft,aes(x = Dataset, y = Frequency)) + 
-  #   geom_bar(aes(fill = Model),stat = "identity") +
-  #   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  #   ggtitle(paste("Majority voting for sample", sample.name,"by criterion",criterion))
-  # dev.off()
-
-  
-  png(paste0(sample.name, "-", criterion, ".barplot.png"))
-  ggplot(ft,aes(x = Dataset, y = Frequency)) + 
-    geom_bar(aes(fill = Model),stat = "identity", position = position_dodge()) +
+  png(paste0(sample.name, "-", criterion, ".png"))
+  ggplot(ft,aes(x = Dataset, y = Frequency)) +
+    geom_bar(aes(fill = Model),stat = "identity") +
     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
     ggtitle(paste("Majority voting for sample", sample.name,"by criterion",criterion))
   dev.off()
+
+  
+  # png(paste0(sample.name, "-", criterion, ".barplot.png"))
+  # ggplot(ft,aes(x = Dataset, y = Frequency)) + 
+  #   geom_bar(aes(fill = Model),stat = "identity", position = position_dodge()) +
+  #   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  #   ggtitle(paste("Majority voting for sample", sample.name,"by criterion",criterion))
+  # dev.off()
 
     
 }
